2c86d218afb71968a1a94ae3c811e2385e45a4d7
irakasleibiltaria <irakasleibiltaria@gmail.com>
2015-05-02 18:51:03 +0200

    Create project02.ino

---
+++ project02.ino
@@ -0,0 +1,39 @@
+// Project 02
+
+int switchState = 0; 
+
+void setup() {
+  // For debug. Tools>Serial Monitor
+  //Serial.begin(9600);
+  // 
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(2,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  switchState = digitalRead(2);
+  //Serial.println(switchState);
+  
+  if (switchState == LOW) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }
+  else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+  
+    delay(250);
+  
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+ 
+    delay(250);
+   
+  }
+  
+}
